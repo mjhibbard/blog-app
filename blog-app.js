564e936673ddd45500512fa9bcac87b3bdda5241
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-05-27 17:44:15 -0700

    inital commit

---
+++ blog-app.js
@@ -0,0 +1,21 @@
+//blog app
+
+//mkdir RESTfulBlogApp <--- cd into
+//npm install express mongoose ejs body-parser --save
+//npm init
+	//entry to app.js
+//touch app.js
+//mkdir views
+//touch views/index.ejs
+//mkdir views/partials
+//touch views/partials/header footer
+//ADDING SEMANTIC UI  ==========
+//  semantic-ui.com or use CDN ----- could use piecewise----
+//mkdir public
+//mkdir public/stylesheets
+//touch public/stylesheets/app.css
+//touch views/new.ejs
+//touch views/show.ejs
+//touch views/edit.ejs
+//npm install method-override --save
+//npm install express-sanitizer --save
